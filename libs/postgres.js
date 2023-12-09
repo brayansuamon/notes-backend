db000152a724a0b1b6f249512caed795fc01eef0
brayansuamon <brayansuamon@gmail.com>
2023-12-08 19:16:39 -0500

    Created docker and postgres db

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,16 +1,16 @@
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// async function getConnection() {
-//   const client = new Client({
-//     host: 'localhost',
-//     port: 5432,
-//     user: 'brayansuamon',
-//     password: 'admin123',
-//     database: 'my_store',
-//   });
-//   await client.connect();
-//   //We need to return to offer the posiblity to execute queries
-//   return client;
-// }
+async function getConnection() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'brayansuamon',
+    password: 'admin123',
+    database: 'my_store',
+  });
+  await client.connect();
+  //We need to return to offer the posiblity to execute queries
+  return client;
+}
 
-// module.exports = getConnection;
+module.exports = getConnection;
