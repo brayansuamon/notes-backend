b32c6ff0f72c3973c18ee2b11487411ce090dc68
brayansuamon <brayansuamon@gmail.com>
2023-12-21 12:04:19 -0500

    Added a role for the db-user using add-role migration and sending the information with insomnia

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,18 +4,21 @@ const userId = Joi.string();
 const email = Joi.string().email();
 const password = Joi.string().min(8);
 const createdAt = Joi.string().isoDate();
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   userId,
   email: email.required(),
   password: password.required(),
   createdAt,
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   email,
   password,
   createdAt,
+  role,
 });
 
 const getUserSchema = Joi.object({
