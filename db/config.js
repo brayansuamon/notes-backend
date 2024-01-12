4bf530b659bb1f53af7c0e9fa7d3dff51e34b68d
brayansuamon <brayansuamon@gmail.com>
2024-01-12 09:57:44 -0500

    Adding config to deploy on render

--- db/config.js
+++ db/config.js
@@ -4,7 +4,8 @@ const { config } = require('../config/config');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 //In the URI we paste the url of connection from AWS or Heroku || https://node-postgres.com/features/connecting
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}/${config.dbName}`;
 
 module.exports = {
   development: {
