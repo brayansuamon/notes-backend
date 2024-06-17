0214efc22d2d2a903d6befaf85e49f13abe2757b
Brayan <brayansuamon@gmail.com>
2024-06-17 12:48:05 -0500

    Implemented change password

--- services/authService.js
+++ services/authService.js
@@ -80,5 +80,20 @@ class AuthService {
     const rta = await this.sendEmail(mail);
     return rta;
   }
+
+  async changePassword(token, newPassword) {
+    try {
+      const payload = jwt.verify(token, secret);
+      const user = await service.findOne(payload.sub);
+      if (user.recoveryToken !== token) {
+        throw boom.unauthorized();
+      }
+      const hash = await bcrypt.hash(newPassword, 10);
+      await service.update(user.id, { password: hash, recoveryToken: null });
+      return { message: 'Password changed' };
+    } catch (error) {
+      throw boom.unauthorized();
+    }
+  }
 }
 module.exports = AuthService;
